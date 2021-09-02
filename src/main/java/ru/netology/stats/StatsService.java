8679668caccbfd3492332f85eb7ae06fc770db12
OlgaMikhaylyukova <olele@inbox.ru>
2021-09-02 17:07:17 +0300

    Modification

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -18,36 +18,58 @@ public class StatsService {
 
     //номер месяца, в котором был пик продаж
     public int maxSales(int[] sales) {
-        int maxMonth = 0;
-        int month = 0;
+        int max = max(sales);
+        int monthCounter = 0;
+        int maxMonthNumber = 0;
         for (int sale : sales) {
-            if (sale >= sales[maxMonth]) {
-                maxMonth = month;
+            monthCounter++;
+            if (sale == max) {
+                maxMonthNumber = monthCounter;
             }
-            month = month + 1;
+                    }
+        return maxMonthNumber ;
+    }
+
+    private int max(int[] sales) {
+        int max = sales[0];
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
         }
-        return maxMonth + 1;
+        return max;
     }
 
     //номер месяца, в котором был минимум продаж
     public int minSales(int[] sales) {
-        int minMonth = 0;
-        int month = 0;
+        int min = min(sales);
+        int monthCounter = 0;
+        int minMonthNumber = 0;
         for (int sale : sales) {
-            if (sale <= sales[minMonth]) {
-                minMonth = month;
+            monthCounter++;
+            if (sale == min) {
+                minMonthNumber = monthCounter;
             }
-            month = month + 1;
         }
-        return minMonth + 1;
+        return minMonthNumber ;
+    }
+
+    private int min(int[] sales) {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
     }
 
     //количество мес, в которых продажи ниже среднего
     public int monthsAmountSalesBelowAverage(int[] sales) {
         int monthsAmount = 0;
-
+        int averageMonthlySales = averageMonthlySales(sales);
         for (int monthSale : sales) {
-            if (monthSale < averageMonthlySales(sales)) {
+            if (monthSale < averageMonthlySales) {
                 monthsAmount += 1;
             }
         }
@@ -57,13 +79,12 @@ public class StatsService {
     //количество мес, в которых продажи выше среднего
     public int monthsAmountSalesAboveAverage(int[] sales) {
         int monthsAmount = 0;
-
+        int averageMonthlySales = averageMonthlySales(sales);
         for (int monthSale : sales) {
-            if (monthSale > averageMonthlySales(sales)) {
+            if (monthSale > averageMonthlySales) {
                 monthsAmount += 1;
             }
         }
         return monthsAmount;
-
     }
 }
